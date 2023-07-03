ebf05c23902ba428f2da41d738f26b46e6e56c55
German Guaigua <gerguaigua@gmail.com>
2023-07-03 09:00:02 -0300

    Fix :beetle: clean client

--- javascript/libs/s3Client.js
+++ javascript/libs/s3Client.js
@@ -1,9 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const REGION = "br02";
-const ENDPOINT = "https://br02-obstveeam01.uni.cloud:443";
-const ACCESS_KEY_ID = "a526f0f32ed240e1a4c09df04b0d5eaa";
-const SECRET_ACCESS_KEY = "ddc13cf862594fcba1da9293b742cda1";
+const ENDPOINT = "";
+const ACCESS_KEY_ID = "";
+const SECRET_ACCESS_KEY = "";
 
 const s3Client = new S3Client({ 
     region: REGION,
